9a717b906b96cc7cb0a51b0eefa5f0228ac682de
FrankMerin <61675381+FrankMerin@users.noreply.github.com>
2020-09-24 21:25:26 -0400

    Hide and Show feature setup for password hiding

--- content.js
+++ content.js
@@ -1,22 +1,33 @@
 let inputs = document.querySelectorAll(
-  'input[name="user"], input[name="username"], input[name="email"], input[name="userID"]'
+  'input[name="user"], input[name="username"], input[name="email"], input[name="userID"], \
+  input[id="user"], input[id="username"], input[id="email"], input[ud="userID"], \
+  input[name=userLoginId]'
 );
 
-for (const input of inputs) {
-  input.setAttribute("type", "password");
-  showButton(input);
+for (const node of inputs) {
+  node.setAttribute("type", "password");
+  showButton(node);
 }
 
 function showButton(node) {
   const rect = node.getBoundingClientRect();
   let buttonOverlay = document.createElement("button");
-  buttonOverlay.style.transform = `translate(${rect.x}px, ${rect.y}px)`;
+  buttonOverlay.style.transform = `translate(${rect.x + 0.7 * rect.width}px, ${rect.y + 0.3 * rect.height}px)`;
   buttonOverlay.id = "buttonOverlay";
   buttonOverlay.textContent = "Show";
   document.body.appendChild(buttonOverlay);
+  buttonOverlay.addEventListener("click", () => showButtonClicked(buttonOverlay, node));
 }
 
-function buttonClicked() {}
+function showButtonClicked(buttonOverlay, node) {
+  if (node.type === "password") {
+    node.setAttribute("type", "text");
+    buttonOverlay.textContent = "Hide";
+  } else {
+    buttonOverlay.textContent = "Show";
+    node.setAttribute("type", "password");
+  }
+}
 
 function contentToBlock() {}
 
@@ -26,3 +37,5 @@ function contentToBlock() {}
 
 // user can input a name they want hidden, and the replacement they want,
 // searches pages and replaces
+
+// window resize listener
